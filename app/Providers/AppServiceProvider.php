d5e1f98af4b8df17f0e2574f0631aef89b583b0a
AlbertWebs <albertmuhatia@gmail.com>
2026-06-17 12:51:48 +0300

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Vite::useHotFile(storage_path('vite.hot'));
+
         RateLimiter::for('client-portal', function (Request $request) {
             return Limit::perMinute(8)->by($request->ip());
         });
